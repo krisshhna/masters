908fb7cd2625243375bd923a2a88ab2c0c15ff2c
krisshhna <krishnayera@gmail.com>
2025-08-20 06:51:45 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'}, // this is empty path match.
+  {
+    path:'home',
+    loadChildren:()=>import('./module/home/home.module').then((a)=>a.HomeModule)
+  },
+  {
+    path:'tailor',
+    loadChildren:()=>import('./module/tailor/tailor.module').then((x)=>x.TailorModule)
+  },
+  {
+    path:'poolTable',
+    loadChildren:()=>import('./module/pool-table/pool-table.module').then((x)=>x.PoolTableModule),
+  },
+  {
+    path:'classes',
+    loadChildren:()=>import('./module/classes/classes.module').then((y)=>y.ClassesModule)
+  },
+  {path:'**', redirectTo:'home'} // this is wild card routing.
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
