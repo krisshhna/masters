7b6cb3254dea6278b4aa025253b10dcff20fbe44
krisshhna <krishnayera@gmail.com>
2025-08-20 19:23:20 +0530

    main files

---
+++ src/app/module/pool-table/pool-table.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PoolTableRoutingModule } from './pool-table-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PoolTableRoutingModule
+  ]
+})
+export class PoolTableModule { }
---
+++ src/app/module/tailor/tailor.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TailorRoutingModule } from './tailor-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TailorRoutingModule
+  ]
+})
+export class TailorModule { }
